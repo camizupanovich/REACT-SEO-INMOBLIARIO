b68d05b4a6d0ff82c8123762a384aacf9be1b7c1
camizupa <cami.zupanovich@hotmail.com>
2025-10-30 23:00:29 -0300

    fix warns

--- src/backoffice/database-service.ts
+++ src/backoffice/database-service.ts
@@ -1,47 +1,47 @@
 // src/backoffice/DatabaseService.ts
-import { ref, set, push, get, child, remove, update } from "firebase/database";
-import { db } from "./firebase";
+import { ref, set, push, get, remove, update } from "firebase/database";
+import { database } from "./firebase";
 import type { PropertyAdvanced } from "../data/mock-properties-advanced";
 import type { BlogPost } from "../data/mock-blog";
 
 // --- Propiedades ---
 export const addProperty = async (property: PropertyAdvanced) => {
-  const newRef = push(ref(db, "properties"));
+  const newRef = push(ref(database, "properties"));
   await set(newRef, property);
   return newRef.key;
 };
 
 export const updateProperty = async (id: string, property: Partial<PropertyAdvanced>) => {
-  await update(ref(db, `properties/${id}`), property);
+  await update(ref(database, `properties/${id}`), property);
 };
 
 export const deleteProperty = async (id: string) => {
-  await remove(ref(db, `properties/${id}`));
+  await remove(ref(database, `properties/${id}`));
 };
 
 export const getProperties = async (): Promise<{ id: string; data: PropertyAdvanced }[]> => {
-  const snapshot = await get(ref(db, "properties"));
+  const snapshot = await get(ref(database, "properties"));
   const data = snapshot.val() || {};
   return Object.keys(data).map((key) => ({ id: key, data: data[key] }));
 };
 
 // --- Blog ---
-export const addBlogPost = async (post: BlogPost) => {
-  const newRef = push(ref(db, "blog"));
+export const addatabaselogPost = async (post: BlogPost) => {
+  const newRef = push(ref(database, "blog"));
   await set(newRef, post);
   return newRef.key;
 };
 
 export const updateBlogPost = async (id: string, post: Partial<BlogPost>) => {
-  await update(ref(db, `blog/${id}`), post);
+  await update(ref(database, `blog/${id}`), post);
 };
 
 export const deleteBlogPost = async (id: string) => {
-  await remove(ref(db, `blog/${id}`));
+  await remove(ref(database, `blog/${id}`));
 };
 
 export const getBlogPosts = async (): Promise<{ id: string; data: BlogPost }[]> => {
-  const snapshot = await get(ref(db, "blog"));
+  const snapshot = await get(ref(database, "blog"));
   const data = snapshot.val() || {};
   return Object.keys(data).map((key) => ({ id: key, data: data[key] }));
 };
